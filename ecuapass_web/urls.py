d970e3618103480cbf838edb62ade21a70108005
lgarreta <lgarreta@gmail.com>
2024-01-20 15:32:12 -0500

    Fixed root url

--- ecuapass_web/urls.py
+++ ecuapass_web/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('creador/', include ("creador.urls")),
     path('admin/', admin.site.urls),
+    path('', include ("creador.urls")),
+    #path('creador/', include ("creador.urls")),
 ]
